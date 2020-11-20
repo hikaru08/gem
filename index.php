895d31efb0c91b0d6f890409d3dfa90ac98e4e59
ARK0730 <tk.ark0730@gmail.com>
2020-11-21 00:34:24 +0900

    commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Gemテンプレート</title>
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+</head>
+<body>
+  <div id="container">
+    <div class="answer">
+      <h2>Button Template</h2>
+      <button class="btn float">Button</button>
+      <button class="btn filled">Button</button>
+      <button class="btn letter-spacing">Button</button>
+      <button class="btn shadow">Button</button>
+      <button class="btn solid">Button</button>
+    </div>
+  </div>
+</body>
+
+</body>
+</html>
