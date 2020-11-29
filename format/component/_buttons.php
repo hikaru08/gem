60e36b473b46e680792e006380620df423ad52f7
Syoma Iwauchi <syoma4645@gmail.com>
2020-11-29 13:30:51 +0900

    format update2

--- format/component/_buttons.php
+++ format/component/_buttons.php
@@ -1,1 +1,15 @@
-<p>い</p>
+<!-- ボタン関係-->
+<!-- -> カラー別-->
+<div class="u-mbs is-bottom">
+  <a class="c-button" #="#">ボタンテキスト</a>
+  <a class="c-button is-secondary" #="#">ボタンテキスト</a>
+  <a class="c-button is-accent" #="#">ボタンテキスト</a>
+  <a class="c-button is-white" #="#">ボタンテキスト</a>
+</div>
+<!-- -> サイズ別-->
+<div class="u-mbs is-bottom">
+  <a class="c-button is-xlg" #="#">ボタンテキスト</a>
+  <a class="c-button is-lg" #="#">ボタンテキスト</a>
+  <a class="c-button is-sm" #="#">ボタンテキスト</a>
+  <a class="c-button is-xs" #="#">ボタンテキスト</a>
+</div>
--- header.php
+++ header.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="ja">
   <head>
+    <?php wp_head(); ?>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/reset.css" />
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/index.css" />
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/scss/style.css">
+    <title>sample</title>
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri()?>/images/favicon.ico" type="image/vnd.microsoft.icon">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;700;900&display=swap" rel="stylesheet">
     <title>Template</title>
-    <?php wp_head(); ?>
   </head>
   <body>
     <header>
