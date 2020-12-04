a4ba2213ea30cc515d3257bf4f241dab193869b7
Syoma Iwauchi <syoma4645@gmail.com>
2020-12-04 23:09:38 +0900

    offer create

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
   <?php wp_footer(); ?>
+  <?php get_template_part('template/layout/_offer');?>
   <!-- ↓どちらかから1つを選択 -->
   <?php get_template_part('template/layout/_footer-minimal');?>
   <!-- <?php get_template_part('template/layout/_footer-simple');?> -->
---
+++ template/layout/_offer.php
@@ -0,0 +1,42 @@
+<!--普通bar l_offer-->
+<div class="l-offer is-color-primary">
+  <div class="l-container">
+    <div class="l-offer__inner">
+      <h2 class="c-heading is-xlg is-white is-bottom">
+        <span>CONTACT</span>
+        <small>お問い合わせ</small>
+      </h2>
+      <div class="l-offer__text">弊社のサービスへ興味をお持ちの方は、<br>下記の電話番号、もしくはお問い合わせフォームよりお気軽に問い合わせください。</div>
+      <div class="l-offer__items">
+        <div class="row">
+          <div class="large-5 is-push-lg-1 small-12">
+            <div class="l-offer__box">
+              <div class="l-offer__box-text">お電話の際は「ホームページを見て」と<br>お伝えください。</div>
+              <div class="l-offer__box-tel">
+                <i class="fa fa-phone" aria-hidden="true"></i>00-000-0000<small>電話受付時間 / 平日00：00～00：00</small>
+              </div>
+            </div>
+          </div>
+          <div class="large-5 small-12">
+            <div class="l-offer__box">
+              <div class="l-offer__box-text">メールでのご相談・ご質問は<br>お問い合わせフォームをご活用ください。</div>
+              <a class="c-button is-lg is-white" #="#">お問い合わせフォーム</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!--省略bar-->
+<!-- <div class="l-offer is-padding-lg">
+  <div class="l-container">
+    <div class="l-offer__inner">
+      <h2 class="c-heading is-md is-bottom">エントリー</h2>
+      <div class="l-offer__text">●●では、働く環境を考え、一緒に課題に挑める仲間を募集しています。<br>私たちと一緒に働いてみませんか？</div>
+      <div class="l-offer__items is-button-one">
+        <a class="l-offer__button is-bgcolor-primary" href="/">エントリーはこちら</a>
+      </div>
+    </div>
+  </div>
+</div> -->
