98da038abff0b46244d31be5b904d44dec5f76be
ARK0730 <tk.ark0730@gmail.com>
2020-12-05 10:35:09 +0900

    commit

--- format/component/_araki1.php
+++ format/component/_araki1.php
@@ -1,10 +1,8 @@
-<div id="container">
-  <div class="answer">
-    <h2>Button Template</h2>
-    <button class="c-button float">ボタンテキスト</button>
-    <button class="c-button filled">ボタンテキスト</button>
-    <button class="c-button letter-spacing">ボタンテキスト</button>
-    <button class="c-button shadow">ボタンテキスト</button>
-    <button class="c-button solid">ボタンテキスト</button>
-  </div>
+<div class="u-mbs is-bottom">
+  <h2>Button Template</h2>
+  <button class="c-button float">ボタンテキスト</button>
+  <button class="c-button filled">ボタンテキスト</button>
+  <button class="c-button letter-spacing">ボタンテキスト</button>
+  <button class="c-button shadow">ボタンテキスト</button>
+  <button class="c-button solid">ボタンテキスト</button>
 </div>
